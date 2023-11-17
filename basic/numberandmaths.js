dd0e229c28b32c84c2a675a73b6ab7ff6d3f7810
rohit98321 <150909209+rohit98321@users.noreply.github.com>
2023-11-17 05:26:05 +0000

    mathandnumber

---
+++ basic/numberandmaths.js
@@ -0,0 +1,30 @@
+const number=100
+
+//console.log(number);
+
+const balance=new Number(number)
+//console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber=123.7656
+//console.log(otherNumber.toPrecision(3));
+
+const thousand=100000
+//console.log(thousand.toLocaleString());
+
+// ====================maths==================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log("this is round figure value" ,Math.round(4.4));
+// console.log("this is floor value ",Math.floor(4.6));
+// console.log("this is upper value ",Math.ceil(4.4));
+// console.log(Math.min(5,7,9,37,2));
+// console.log(Math.max(5,7,9,37,2));
+
+console.log(Math.random());
+let r=Math.random()*100
+console.log(Math.floor(r));
+
